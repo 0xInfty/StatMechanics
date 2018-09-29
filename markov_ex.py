c75ad2d5e9fc06ed3db3c746486726ed59bcc1dc
0xInfty <anirix.vall@gmail.com>
2018-09-28 21:37:44 -0300

    Script 'markov_ex' [all]. Changes: 1 Functional (+) + 3 Syntactic (+).
    Changes
    -------
    
    Functional (+):
    	erased 'Mc', 'Q' and 'R' matrix because I now use 'markov_canonic_find()' and 'markov_canonic_decompose()'.
    
    Syntactic (+):
    	changed 'M0' to 'M';
    	changed 'mkv.get_canonic_data' to 'mkv.canonic_data' because of 'markov' module's changes.
    
    Syntactic (-):
    	translated documentation.

--- markov_ex.py
+++ markov_ex.py
@@ -2,8 +2,14 @@
 """
 This script includes some examples on how to use 'markov' module.
 
-Some resolved exercises:
-   Ej. 1: Problema con estados absorventes del borracho discreto
+Some solved exercises:
+
+Ex.1: 
+    3-steps drunk man's absorvant states problem.
+Ex.2:
+    Prisoner's 7-steps bet absorvant states problem.
+   
+See also 'markov.py' on https://github.com/0xInfty/StatMechanics.git
 
 @author: 0xInfty
 @date: 09/2018
@@ -12,58 +18,51 @@ Some resolved exercises:
 import numpy as np
 import markov as mkv
 
-#%% Ej. 1: Problema con estados absorventes del borracho discreto
+#%% Ex.1: 3-steps drunk man's absorvant states problem
 
-"""Problema del borracho discreto con 3 pasos intermedios.
+"""Drunk man's absorvant states problem (3 mid steps).
 
-Cuatro cuadras separan de un bar la casa de un hombre.
+There are four blocks between the bar and the drunk man's home.
 
-casa____1_____2_____3_____bar
+home____1_____2_____3_____bar
 
-Si el hombre se encuentra en cualquiera de las tres esquinas 
-intermedias, se dirigirá hacia su casa o hacia el bar con igual 
-probabilidad. Pero si llega, se quedará en uno de los dos sitios.
+If the man is at any of the three middle corners, he will walk towards 
+the bar or his home with the same probability. But if he arrives at 
+either the bar or his home, he will stay there.
 
-a) Encuentre la matriz  de transición del problema. Exprésela de forma
-canónica.
-b) ¿Cuánto tiempo caminará el hombre antes de detenerse?
-c) ¿Con qué probabilidad terminará en el bar?
+a) Find the transition matrix and its associated canonic matrix.
+b) How much will the man walk befor he stops?
+c) What is the probability that he will end at the bar?
 
 """
 
-M0 = np.zeros([5,5]) # transition matrix (0 is home, 4 is the bar)
+M = np.zeros([5,5]) # transition matrix (0 is home, 4 is the bar)
 for i in [0,4]:
-    M0[i,i] = 1
+    M[i,i] = 1
 for i in [1,2,3]:
-    M0[i-1,i] = 0.5
-    M0[i+1,i] = 0.5
+    M[i-1,i] = 0.5
+    M[i+1,i] = 0.5
 
-Mc = np.zeros([5,5]) # canonical matrix
-Q = .5 * (np.diag(np.ones(2), -1) + np.diag(np.ones(2), 1))
-R = np.zeros([2,3]) # 2 absorvant states and 3 transitory states
-R[0,0] = .5
-R[1,2] = .5
-Mc[:3,:3] = Q # the right upper part of Mc is Q
-Mc[3:,:3] = R # the right lower part of Mc is R
-Mc[3:,3:] = np.diag(np.ones(2)) # the left lower part of Mc is 1
-  
+
+Mc, ab = mkv.canonic_finder(M) # canonic matrix with ab absorvant states
+Q, R = mkv.canonic_decomposer(Mc, ab) # QR decomposition
 N, RN = mkv.canonic_solver(Q, R) # apply the N = (1-Q)^(-1) method
 
-ans_b = [mkv.get_canonic_data(N, RN, "abs", 1),
-         mkv.get_canonic_data(N, RN, "abs", 2),
-         mkv.get_canonic_data(N, RN, "abs", 3)]
+print("\n")
+ans_b = [mkv.canonic_data(N, RN, "abs", 1),
+         mkv.canonic_data(N, RN, "abs", 2),
+         mkv.canonic_data(N, RN, "abs", 3)]
 
-ans_c = [mkv.get_canonic_data(N, RN, "pr", 1, 2),
-         mkv.get_canonic_data(N, RN, "pr", 2, 2),
-         mkv.get_canonic_data(N, RN, "pr", 3, 2)]
+print("\n")
+ans_c = [mkv.canonic_data(N, RN, "pr", 1, 2),
+         mkv.canonic_data(N, RN, "pr", 2, 2),
+         mkv.canonic_data(N, RN, "pr", 3, 2)]
+
   
-#%% Ej.2: Resolver un problema más grande de estados absorventes.
+#%% Ex.2: Prisoner's 7-steps bet absorvant states problem.
 
 M = np.diag(np.ones(8),-1)*.6 + .4*np.diag(np.ones(8),1)
 M[:,0] = np.zeros(9)
 M[:,8] = np.zeros(9)
 M[0,0] = 1
 M[8,8] = 1
- 
-Mc = np.zeros([9,9])
-Mc[0:6,0:6] = np.diag(np.ones(8))
