16c6d5d38c04ef06b94472d6f85d828c72c00e3a
0xInfty <anirix.vall@gmail.com>
2018-09-28 21:30:57 -0300

    Module 'markov' ['canonic_finder', 'canonic_decomposer'].
    ---> 'canonic_finder' function
    
    	This function finds 'Mc' Markov canonic matrix of transition probabilities for a given 'M' Markov transition matrix, whose elements 'M[i,j]' hold the transition probability for
    going from 'j' state to 'i' state. Also returns the number 'ab' of absorvant states.
    
    ---> 'canonic_decomposer' function
    
    	This function decomposes 'Mc' Markov canonic matrix of transition probabilities into 'Q' and 'R' matrix. 'Q[i,j]' holds the transition probability from 'j' transient state to
    'i' transient state. 'R[i,j]' holds the transition probabily from 'j' transient state to 'i' absorvant state.

--- markov.py
+++ markov.py
@@ -3,16 +3,18 @@
 This module contains functions to solve Markov's probability problems.
 
 Some of its useful functions are:
-    
-solve_canonic():
-    Returns N and R.N from Q, R extracted from M canonical matrix.
-get_canonic_data():
+
+canonic_finder():
+    Finds 'Mc' canonic matrix from 'M' transition probabilities matrix.
+canonic_decomposer():
+    Returns 'Q, R' matrix from 'Mc' Markov canonic transition matrix.
+canonic_solver():
+    Returns 'N' and 'R.N' from 'Q, R' partial Markov transition matrix.
+canonic_data():
     Searches data on 'N', 'RN' of Markov's 'M' with absorvant states.
 
-Some of examples:
-    
-1) Solve a Markov problem with absorvant states.
-
+There are some examples of its use available at 'markov_ex.py' from 
+https://github.com/0xInfty/StatMechanics.git
 
 @author: 0xInfty
 @date: 09/2018
@@ -62,6 +64,184 @@ def counting_suffix(number):
     return ans
  
 #%% Module corpus
+
+def canonic_finder(M):
+
+    """From 'M' with 'ab' absorvant states, returns 'Mc' canonic matrix.
+    
+    It takes 'M' as the Markov matrix of transition probabilities, whose 
+    elements 'M[i,j]' hold the transition probability of going from 'j' 
+    state to 'i' state. It identifies the absorvant states and 
+    constructs the change of base matrix 'C' from a list of permutation 
+    pairs. Then, it defines 'Mc' as the Markov canonic matrix of 
+    transition. Finally, it returns 'Mc' and 'tr'.
+    
+    Parameters
+    ----------
+    M: numpy.array of size 'm'
+        Square matrix of Markov transition probabilities. It has size 
+        'mxm', where 'm' is the number of states.
+    
+    Returns
+    -------
+    Mc: numpy.array of size 'm'
+        Square Markov canonic matrix of transition probabilities. It 
+        also has size 'mxm', same as M.
+    ab: int
+        Number of absorvant states.
+    
+    Raises
+    ------
+    TypeError("M should be a matrix"):
+        if M is not a matrix.
+    TypeError("M should be a square matrix"):
+        if M is not a square matrix.
+    ValueError("M's determinat should be 0"):
+        if M's determinant is not zero.
+    ValueError("M's columns should each add up to 1")
+        if any of M's column doesn't add up to 1, because in that case 
+        the transition probabilities from a single state, ordered on M's 
+        columns, don't add up to 1.
+    ValueError("No absorvant states found on M"):
+        if ab=0, since in that case Mc can't be defined.
+        
+    """
+    
+    try:
+        m = len(M[:,0])
+    except IndexError:
+        raise TypeError("M should be a matrix")
+        return
+    
+    if m != len(M[0,:]):
+        raise TypeError("M should be a square matrix")
+        return
+
+    if np.linalg.det(M) != 0:
+        raise ValueError("M's determinat should be 0")
+        return    
+    
+    if not (np.dot(np.ones(m),M) == np.ones(m)).all():
+        raise ValueError("M's columns should each add up to 1")
+        return
+        
+    absorvant_states_list = []
+    ab = 0 # number of absorvant states
+    for i in range(m):
+        versor = np.zeros(m)
+        versor[i] = 1
+        if (M[:,i] == versor).all():
+            print("State 'M{}' is {} absorvant state".format(
+                  i, counting_suffix(ab+1)))
+            absorvant_states_list.append(i)
+            ab = ab + 1
+    
+    tr = 0 # number of transient states
+    for i in range(m):
+        if i not in absorvant_states_list:
+            print("State 'M{}' is {} transient state".format(
+                  i, counting_suffix(tr+1)))
+            tr = tr + 1
+    
+    if ab != 0:
+                
+        j_ab = tr
+        j_tr = 0
+        change_of_base_pairs = []
+        for i in range(m):
+            if i in absorvant_states_list:
+                change_of_base_pairs.append((i,j_ab))
+                j_ab = j_ab + 1
+            else:
+                change_of_base_pairs.append((i,j_tr))
+                j_tr = j_tr + 1
+                
+        C = np.zeros([m,m]) # change of base matrix
+        
+        for (i, j) in change_of_base_pairs:
+            print("State 'M{}' will be 'C{}'".format(i,j))
+            C[i,j] = 1
+        
+        Mc = np.dot(np.transpose(C), np.dot(M,C)) # canonic matrix
+        
+    else:
+        
+        raise ValueError("No absorvant states found on M")
+        return               
+    
+    return Mc, ab
+
+def canonic_decomposer(Mc, ab):
+    
+    """Decomposes 'Mc' Markov canonic matrix with 'ab' absorvant states.
+    
+    It takes 'Mc' as the Markov canonic matrix of transition 
+    probabilities with 'ab' absorvant states. Then, it decomposes it on 
+    'Q' and 'R' matrix. 'Q' only holds the transition probabilities from 
+    and to transient states. 'R' only holds the transition 
+    probabilities from transient states to absorvant states.
+    
+    Parameters
+    ----------
+    Mc: numpy.array of size 'm'
+        Square Markov canonic matrix of transition probabilities. It has 
+        size 'mxm', where 'm' is the total number of states.
+    ab: int
+        Number of absorvant states.
+    
+    Returns
+    -------
+    Q: numpy.array of size '(tr,tr)'
+        Square matrix of transition probabilities from and to transient 
+        states. It has size 'trxtr', where 'tr' is the number of 
+        transient states.
+    R: numpy.array of size '(ab,tr)'
+        Partial matrix of transition probabilities from transient states 
+        to absorvant states. It has size 'abxtr', where 'ab' is the 
+        number of absorvant states and 'tr' is the number of transient 
+        states.
+    
+    Raises
+    ------
+    TypeError("Mc should be a matrix"):
+        if Mc is not a matrix.
+    TypeError("Mc should be a square matrix"):
+        if Mc is not a square matrix.
+    ValueError("Mc's determinat should be 0"):
+        if Mc's determinant is not zero.
+    ValueError("Mc's columns should each add up to 1")
+        if any of Mc's column doesn't add up to 1, because in that case 
+        the transition probabilities from a single state, ordered on 
+        Mc's columns, don't add up to 1.
+        
+    """
+    
+    try:
+        m = len(Mc[:,0])
+    except IndexError:
+        raise TypeError("Mc should be a matrix")
+        return
+    
+    if m != len(Mc[0,:]):
+        raise TypeError("Mc should be a square matrix")
+        return
+
+    if np.linalg.det(Mc) != 0:
+        raise ValueError("Mc's determinat should be 0")
+        return    
+    
+    if not (np.dot(np.ones(m),Mc) == np.ones(m)).all():
+        raise ValueError("Mc's columns should each add up to 1")
+        return
+    
+    tr = m - ab # number of transient states
+
+    Q = Mc[:tr,:tr] # the right upper part of Mc is Q
+    R = Mc[tr:,:tr] # the right lower part of Mc is R
+    # the left lower part of Mc is 1
+    # the left upper part of Mc is 0
+    
+    return Q, R
 
 def canonic_solver(Q, R):
     
@@ -137,6 +317,7 @@ def canonic_solver(Q, R):
     RN = np.dot(R, N)
     
     return N, RN
+
 
 def canonic_data(N, RN, datastring, index_from_1, 
                  index_to_1=None, print_result=True):
